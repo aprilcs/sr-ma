43a35ae85bf16a02d4331a08b4a660b268afc7b6
April <aprilcs@users.noreply.github.com>
2020-02-05 00:48:10 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,6 +1,6 @@
 install.packages("tidyverse")
 install.packages("meta")
 install.packages("metafor")
-install.packages ("devtools")
-devtools::install_github('CenterForOpenScience/osfr')
+# install.packages ("devtools")
+# devtools::install_github('CenterForOpenScience/osfr')
 
